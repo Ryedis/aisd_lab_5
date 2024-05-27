553fdeeee448cba8c8373458b21e1203f9d94167
Ryedis <134209766+Ryedis@users.noreply.github.com>
2024-05-28 00:52:15 +0400

    add pearson hash

--- map_tests.cpp
+++ map_tests.cpp
@@ -33,8 +33,12 @@ TEST(CustomMapTest, CopyConstructor) {
 TEST(CustomMapTest, AssignmentOperator) {
     Map<int, int> map1(10);
     map1.insert(11, 5);
+    map1.insert(12, 5);
+    map1.insert(13, 5);
+    map1.insert(14, 5);
+    map1.insert(15, 5);
 
-    Map<int, int> map2(10);
+    Map<int, int> map2(3);
     map2 = map1;
 
     ASSERT_TRUE(map2.contains(11));
@@ -68,12 +72,30 @@ TEST(CustomMapTest, EraseMethod) {
     ASSERT_FALSE(map.contains(4));
 }
 
-TEST(CustomMapTest, SearchTest) {
+TEST(CustomMapTest, Search) {
     Map<int, int> map(10);
     map.insert(4, 12);
     
     EXPECT_EQ(*map.search(4) , 12);
     EXPECT_EQ(map.search(5), nullptr);
+}
+
+TEST(CustomMapTest, RandomConstructor) {
+    Map<int, string> map(10, 15);
+
+    map.print();
+}
+
+TEST(CustomMapTest, PearsonHash) {
+    string s = "ggxoqkmemtjawqt";
+    cout << pearson_hash(s) << endl;
+}
+
+TEST(CustomMapTest, CompareString) {
+    string s = "ggxoqkmemtjawqt";
+ 
+    ASSERT_TRUE(compare_string(pearson_hash(s), "ggxoqkmemtjawqt"));
+    ASSERT_FALSE(compare_string(pearson_hash(s), "ikoyxaqbgeqwjmk"));
 }
 
 int main(int argc, char** argv) {
